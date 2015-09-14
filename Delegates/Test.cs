80d221a6bc19a978370a69bc773fb71fca7c6e16
Ravindra Kale <ravind999@gmail.com>
2015-09-14 14:08:05 +0530

    Pass delegate as a parameter to function

--- Delegates/Test.cs
+++ Delegates/Test.cs
@@ -6,62 +6,41 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
-    public class Currency
+    public class MathOperatios
     {
-        public uint Dollars;
-        public ushort Cents;
-        public Currency(uint dollars, ushort cents)
+        public static double MultipleByTwo(double value)
         {
-            this.Dollars = dollars;
-            this.Cents = cents;
+            return value * 2;
         }
-        public override string ToString()
+        public static double Square(double value)
         {
-            return string.Format("${0}.{1,-2:00}", Dollars, Cents);
-        }
-        public static string GetCurrencyUnit()
-        {
-            return "Doller";
-        }
-        public static explicit operator Currency(float value)
-        {
-            checked
-            {
-                uint dollars = (uint)value;
-                ushort cents = (ushort)((value - dollars) * 100);
-                return new Currency(dollars, cents);
-            }
-        }
-
-        public static implicit operator float(Currency value)
-        {
-            return value.Dollars + (value.Cents / 100.0f);
-        }
-        public static implicit operator Currency(uint value)
-        {
-            return new Currency(value, 0);
-        }
-        public static implicit operator uint(Currency value)
-        {
-            return value.Dollars;
+            return value * value;
         }
     }
+
     class Test
     {
-        public delegate string GetAString();
+        public delegate double DoubleOp(double x);
         static void Main(string[] args)
         {
-            int x = 40;
-            GetAString fsm = x.ToString;
-            Console.WriteLine("String is {0}", fsm());
-            Currency balance = new Currency(34, 50);
-            fsm = balance.ToString;
-            Console.WriteLine("String is {0}", fsm());
-            fsm = new GetAString(Currency.GetCurrencyUnit);
-            Console.WriteLine("String is {0}", fsm());
-            Currency cur = (uint)55;
-            Console.WriteLine(cur.ToString());
+            DoubleOp[] operations = {
+                                        MathOperatios.MultipleByTwo,
+                                        MathOperatios.Square
+                                    };
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine("Using operations [{0}]", i);
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+                ProcessAndDisplayNumber(operations[i], 25);
+            }
             Console.ReadKey();
+        }
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0}, result of operations is {1}", value, result);
         }
     }
 }
