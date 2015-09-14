bbcbf8a8efea79c8c8dea7f6c794b6ed95c2efcb
Ravindra Kale <ravind999@gmail.com>
2015-09-14 10:51:29 +0530

    Simple Delegate Implemented

---
+++ Delegates/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Math
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    class Test
+    {
+        public delegate int CalculationHandler(int x, int y);
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            CalculationHandler sumHandler = new CalculationHandler(math.Sum);
+            int result = sumHandler(12, 25);
+            Console.WriteLine("Result is: " + result);
+            Console.ReadKey();
+        }
+    }
+}
